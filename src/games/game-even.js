6a2f0fd87e24e90e55a7a2a3eab7d48def7b6d26
Ilya <i.kostin@semrush.com>
2021-10-20 19:00:33 +0300

    fix: game-even.js

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -3,11 +3,13 @@ import newGame from '../index.js';
 
 const gameQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (num) => num % 2 === 0;
+
 const gameQuestionAnswer = () => {
   const maxNumForRundomNumber = 100;
   const rundomNumber = Math.floor(Math.random() * maxNumForRundomNumber);
   const question = rundomNumber;
-  const answer = question % 2 === 0 ? 'yes' : 'no';
+  const answer = isEven(question) ? 'yes' : 'no';
   return [question, answer];
 };
 
