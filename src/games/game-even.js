5c4e7346c874d05800c7723e975ddf3db91ab130
Ilya <i.kostin@semrush.com>
2021-10-20 16:11:37 +0300

    fix: brain-even.js

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -6,7 +6,7 @@ const gameQuestion = 'Answer "yes" if the number is even, otherwise answer "no".
 const gameQuestionAnswer = () => {
   const maxNumForRundomNumber = 100;
   const rundomNumber = Math.floor(Math.random() * maxNumForRundomNumber);
-  const question = `${rundomNumber}`;
+  const question = rundomNumber;
   const answer = rundomNumber % 2 === 0 ? 'yes' : 'no';
   return [question, answer];
 };
