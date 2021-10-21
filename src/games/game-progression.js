0d3599b0f29ffec004ee29cab9c921afb6a7228e
Ilya <i.kostin@semrush.com>
2021-10-21 12:14:51 +0300

    fix

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -12,18 +12,53 @@ const gameQuestionAnswer = () => {
   const maxNumForFirstNumber = 20;
   const firstNumber = Math.floor(Math.random() * maxNumForFirstNumber);
   const startNumber = firstNumber;
-  const startNumberForLength = 1;
-  const endNumberForLength = 9;
-  const randomStep = randomLength(startNumberForLength, endNumberForLength);
-  const rundomHiddenNumber = Math.floor(Math.random() * randomStep);
 
+  const startNumberForLength = 1;
+  const endNumberForLength = 10;
+  const randomStep = randomLength(startNumberForLength, endNumberForLength);
+
+  const rundomHiddenNumber = Math.floor(Math.random() * randomStep);
   const startNumberForHiddenNumber = 5;
   const endNumberForHiddenNumber = 10;
   const hiddenElement = '..';
-
   const lengthForHiddenElement = randomLength(startNumberForHiddenNumber, endNumberForHiddenNumber);
 
-  // const getArithmeticProgression = (start, step, length) => {
+  const getArithmeticProgression = (start, length, step) => {
+    let result = [];
+    let startOfProgression = start;
+    result.push(startOfProgression);
+
+    for (let i = 0; i <= length; i += 1) {
+      let numberOfProgression = startOfProgression + step;
+      startOfProgression = numberOfProgression;
+      if (i === rundomHiddenNumber) {
+        numberOfProgression = hiddenElement;
+      }
+      result.push(String(numberOfProgression));
+    }
+    result = result.join(' ');
+    return result;
+  };
+
+  const getHiddenElement = () => {
+    const hiddenMemberIndex = rundomHiddenNumber + 1;
+    const openHiddenElement = startNumber + randomStep * hiddenMemberIndex;
+    return openHiddenElement;
+  };
+
+  const question = `${getArithmeticProgression(firstNumber, lengthForHiddenElement, randomStep)}`;
+
+  const answer = String(getHiddenElement());
+  return [question, answer];
+};
+
+const brainProgression = () => newGame(gameQuestion, gameQuestionAnswer);
+
+export default brainProgression;
+
+
+
+// const getArithmeticProgression = (start, step, length) => {
   //   let result = [];
   //   const startNumberForHiddenNumber = 5;
   //   const endNumberForHiddenNumber = 10;
@@ -42,36 +77,3 @@ const gameQuestionAnswer = () => {
   //   result = result.join(' ');
   //   return result;
   // };
-
-  const getArithmeticProgression = (start, length, step) => {
-    let result = [];
-    let startOfProgression = start;
-    result.push(startOfProgression);
-
-    for (let i = 0; i <= length; i += 1) {
-      let numberOfProgression = startOfProgression + step;
-      startOfProgression = numberOfProgression;
-      if (i === rundomHiddenNumber) {
-        numberOfProgression = hiddenElement;
-      }
-      result.push(String(numberOfProgression));
-    }
-
-    result = result.join(' ');
-    return result;
-  };
-
-  const getHiddenElement = () => {
-    const openHiddenElement = startNumber + randomStep * (rundomHiddenNumber + 1);
-    return openHiddenElement;
-  };
-
-  const question = `${getArithmeticProgression(firstNumber, lengthForHiddenElement, randomStep)}`;
-
-  const answer = String(getHiddenElement());
-  return [question, answer];
-};
-
-const brainProgression = () => newGame(gameQuestion, gameQuestionAnswer);
-
-export default brainProgression;
